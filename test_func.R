61b755296efbaaf9a8e8989c378b893e67c6c391
Eric Youngson <eric@scneco.com>
2015-09-30 15:38:31 -0700

    Revised usage selection to bring in records related to valid buildings that also have valid usage & cost entries

--- test_func.R
+++ test_func.R
@@ -1,14 +1,13 @@
 test = function(bldgs, usage) {
   bldg.ID = subset(bldgs[,"BuildingID"], c(!(is.na(bldgs$PropertyName)) | !(is.na(bldgs$BusinessName))) & bldgs$LeaseableArea > 0)
-  print(length(bldg.ID))
+  # print(length(bldg.ID))
   
-  bldg.Usage = usage[usage$BuildingID==bldg.ID[1],]
-  test3 = bldg.Usage[duplicated(bldg.Usage),]
-  # test4 = 
+  cln.Usage = usage[!(is.na(usage$EnergyReading)) & !(is.na(usage$EnergyCost)),]
+  bldg.Usage = cln.Usage[cln.Usage$BuildingID==bldg.ID[1],]
+  # test3 = bldg.Usage[duplicated(bldg.Usage),]
+  # test4 = bldg.Usage[bldg.Usage$EnergyReading,1]
   
-  # print(length(bldg.Usage))
-  # print(length(test3))
-  # print(test3)
+  print(length(bldg.Usage[,1])) # bldg.Usage)
 
   return()
 }
