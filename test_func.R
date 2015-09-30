ea1e0178b9d5bbb72abbfdedc4cb10677e07c55d
Eric Youngson <eric@scneco.com>
2015-09-30 15:56:57 -0700

    Started a new test to simply subtract start-time from the previous record to get the span

--- test_func.R
+++ test_func.R
@@ -4,10 +4,11 @@ test = function(bldgs, usage) {
   
   cln.Usage = usage[!(is.na(usage$EnergyReading)) & !(is.na(usage$EnergyCost)),]
   bldg.Usage = cln.Usage[cln.Usage$BuildingID==bldg.ID[1],]
-  # test3 = bldg.Usage[duplicated(bldg.Usage),]
-  # test4 = bldg.Usage[bldg.Usage$EnergyReading,1]
+  test3 = bldg.Usage[duplicated(bldg.Usage),]
+  test4 = as.Date(bldg.Usage[1,"StartDate"]) - as.Date(bldg.Usage[2,"StartDate"])
   
-  print(length(bldg.Usage[,1])) # bldg.Usage)
+  print(typeof(test4))
+  print(as.numeric(test4))
 
   return()
 }
