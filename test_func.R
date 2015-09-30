4a3faf9a512c25731db4b9fac2adefd9616241d2
Eric Youngson <eric@scneco.com>
2015-09-30 12:21:30 -0700

    Implemented the first filter: duplicated records

--- test_func.R
+++ test_func.R
@@ -1,5 +1,10 @@
 test = function(bldgs, usage) {
   bldg.ID = bldgs[,"BuildingID"]
   bldg.Usage = usage[usage$BuildingID==bldg.ID[1],]
-  return(bldg.Usage)
+  test1 = bldg.Usage[duplicated(bldg.Usage),]
+  
+  print(length(bldg.Usage))
+  print(length(test1))
+  print(test1)
+  return()
 }
