91a4dd6a55f46c29c1aa7ef20b3553d998281ad0
Eric Youngson <eric@scneco.com>
2015-09-30 12:47:56 -0700

    Added building criteria: name, leaseable area

--- test_func.R
+++ test_func.R
@@ -1,10 +1,13 @@
 test = function(bldgs, usage) {
-  bldg.ID = bldgs[,"BuildingID"]
-  bldg.Usage = usage[usage$BuildingID==bldg.ID[1],]
-  test1 = bldg.Usage[duplicated(bldg.Usage),]
+  bldg.ID = subset(bldgs[,"BuildingID"], !(bldgs$PropertyName=="") | bldgs$LeaseableArea > 0)
+  print(length(bldg.ID))
   
-  print(length(bldg.Usage))
-  print(length(test1))
-  print(test1)
+  bldg.Usage = usage[usage$BuildingID==bldg.ID[1],]
+  test3 = bldg.Usage[duplicated(bldg.Usage),]
+  
+  # print(length(bldg.Usage))
+  # print(length(test3))
+  # print(test3)
+
   return()
 }
