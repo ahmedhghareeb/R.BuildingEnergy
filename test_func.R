7286c218f58a43919f5c1fda1f65f1f477dfd953
Eric Youngson <eric@scneco.com>
2015-09-30 16:03:39 -0700

    Revised start & end time reads to get span of each entry, added indicators for future loop to run for all buildings

--- test_func.R
+++ test_func.R
@@ -1,14 +1,15 @@
 test = function(bldgs, usage) {
   bldg.ID = subset(bldgs[,"BuildingID"], c(!(is.na(bldgs$PropertyName)) | !(is.na(bldgs$BusinessName))) & bldgs$LeaseableArea > 0)
-  # print(length(bldg.ID))
-  
   cln.Usage = usage[!(is.na(usage$EnergyReading)) & !(is.na(usage$EnergyCost)),]
+  
+  #loop{
   bldg.Usage = cln.Usage[cln.Usage$BuildingID==bldg.ID[1],]
   test3 = bldg.Usage[duplicated(bldg.Usage),]
-  test4 = as.Date(bldg.Usage[1,"StartDate"]) - as.Date(bldg.Usage[2,"StartDate"])
+  spans = as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"]) 
+  #}
   
-  print(typeof(test4))
-  print(as.numeric(test4))
+  print(typeof(spans))
+  print(as.numeric(spans))
 
   return()
 }
