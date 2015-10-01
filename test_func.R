96b69e9b192f0cb61f59d685f128cf16775d4d24
Eric Youngson <eric@scneco.com>
2015-10-01 16:18:35 -0700

    Split elec. & gas usage to seperate dataframes, introduced a loop to iterate through test5 (lagging comparisons)

--- test_func.R
+++ test_func.R
@@ -8,26 +8,33 @@ test = function(bldgs, usage) {
   i = 23
   # Pull valid usage records associated with valid buildings
   bldg.Usage = data.frame(cln.Usage[cln.Usage$BuildingID==bldg.ID[i],])
-  bldg.Usage = bldg.Usage[order(as.Date(bldg.Usage$StartDate)),]
+  
+  bldg.Usage.Elec = bldg.Usage[bldg.Usage$MeterUnits=="kWh",]
+  bldg.Usage.Elec = bldg.Usage.Elec[order(as.Date(bldg.Usage.Elec$StartDate)),]
+  
+  bldg.Usage.Gas = bldg.Usage[bldg.Usage$MeterUnits=="Therm",]
+  bldg.Usage.Gas = bldg.Usage.Gas[order(as.Date(bldg.Usage.Gas$StartDate)),]
+  
   # Test for dupicate entries: returns duplicates (Test 1 & 2 are imbeded in the import section above)
   test3 = bldg.Usage[duplicated(bldg.Usage),]
   # Test for length of each record: returns valid records
   test4 = subset(c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"])),
                  c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"]) > 14) &
                  c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"]) < 60))
-   # Subloop{
-  j = 1
-  test5 =  c(as.Date(bldg.Usage[j,"EndDate"]) - as.Date(bldg.Usage[j+1,"EndDate"]))
-   #}
+   # Subloop
+  len = length(bldg.Usage.Gas[,1])-1
+  print(len)
+  for (j in 1:len) {
+    test5g =  c(as.Date(bldg.Usage.Gas[j,"EndDate"]) - as.Date(bldg.Usage.Gas[j+1,"StartDate"]))
+    test5e =  c(as.Date(bldg.Usage.Elec[j,"EndDate"]) - as.Date(bldg.Usage.Elec[j+1,"StartDate"]))
+    
+    print(c(as.Date(bldg.Usage.Elec[j,"EndDate"])))
+    print(c(as.Date(bldg.Usage.Elec[j+1,"StartDate"])))
+    print(test5e)
+  }
   #}
-
+  
   print(bldg.ID[i])
-  print(typeof(bldg.Usage))
-  print(bldg.Usage)
-  print(as.Date(bldg.Usage[j,"EndDate"]))
-  print(as.Date(bldg.Usage[j+1,"EndDate"]))
-  print(test5)
-  # print(bldg.Usage)
-
+  
   return()
 }
