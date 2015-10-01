cd3f3c6f01f489e4c5d94cb79950cc742e1cda8c
Eric Youngson <eric@scneco.com>
2015-10-01 11:53:31 -0700

    Modified test4 for visual clarity

--- test_func.R
+++ test_func.R
@@ -5,7 +5,8 @@ test = function(bldgs, usage) {
   #loop{
   bldg.Usage = cln.Usage[cln.Usage$BuildingID==bldg.ID[1],]
   test3 = bldg.Usage[duplicated(bldg.Usage),]
-  test4 = subset(c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"])), c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"]) > 14) &
+  test4 = subset(c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"])),
+                 c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"]) > 14) &
                  c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"]) < 60))
   #}
   
