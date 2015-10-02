d3c990f33f6a3d593bac696a43f7b98a27a39795
Eric Youngson <eric@scneco.com>
2015-10-02 15:04:36 -0700

    Revised line length for clarity, saved R Studio project

--- test_func.R
+++ test_func.R
@@ -7,7 +7,7 @@ test = function(bldgs, usage) {
   
   # Integration test for buildings w/o usage
   bldg.ID = bldg.ID[c(bldg.ID %in% cln.Usage$BuildingID)]
-  print(bldg.ID)
+  print(length(bldg.ID))
   
   # Loop{
   i = 5
@@ -21,15 +21,19 @@ test = function(bldgs, usage) {
   bldg.Usage.Gas = bldg.Usage[bldg.Usage$MeterUnits=="Therm",]
   bldg.Usage.Gas = bldg.Usage.Gas[order(as.Date(bldg.Usage.Gas$StartDate)),]
   
-  # Test for dupicate entries: returns duplicates (Test 1 & 2 are imbeded in the import section above)
+  # Test for dupicate entries: returns duplicates
+  # (Test 1 & 2 are imbeded in the import section above)
   test3 = bldg.Usage[duplicated(bldg.Usage),]
   print("Test 3")
   print(test3)
   
   # Test for length of each record: returns valid records
-  test4 = subset(c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"])),
-                 c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"]) < 14) &
-                 c(as.Date(bldg.Usage[,"EndDate"]) - as.Date(bldg.Usage[,"StartDate"]) > 60))
+  test4 = subset(c(as.Date(bldg.Usage[,"EndDate"]) - 
+                   as.Date(bldg.Usage[,"StartDate"])),
+                 c(as.Date(bldg.Usage[,"EndDate"]) -
+                   as.Date(bldg.Usage[,"StartDate"]) < 14) &
+                 c(as.Date(bldg.Usage[,"EndDate"]) -
+                   as.Date(bldg.Usage[,"StartDate"]) > 60))
   print("Test 4")
   print(test4)
   
@@ -37,14 +41,17 @@ test = function(bldgs, usage) {
   len = length(bldg.Usage.Gas[,1])-1
   for (j in 1:len) {
     # Check overlapping days in elec. & gas usage reporting ranges
-    test5g =  c(as.Date(bldg.Usage.Gas[j,"EndDate"]) - as.Date(bldg.Usage.Gas[j+1,"StartDate"]))
-    if (abs(test5g) > 4) {
-      print("Gas")
-      print(bldg.Usage.Gas[j,"MeterID"])
-      print(as.Date(bldg.Usage.Gas[j,"EndDate"]))
+    test5g =  c(as.Date(bldg.Usage.Gas[j,"EndDate"]) -
+                as.Date(bldg.Usage.Gas[j+  1,"StartDate"]))  
+    if (abs(test5g) >   4) {  
+      print("Gas")  
+      print(bldg.Usage.Gas[j,"MeterID"])  
+      print(as.Date(bldg.Usage.Gas[j,"EndDate"])) 
       print(as.Date(bldg.Usage.Gas[j+1,"StartDate"]))
     }
-    test5e =  c(as.Date(bldg.Usage.Elec[j,"EndDate"]) - as.Date(bldg.Usage.Elec[j+1,"StartDate"]))
+
+    test5e =  c(as.Date(bldg.Usage.Elec[j,"EndDate"]) -
+                as.Date(bldg.Usage.Elec[j+1,"StartDate"]))
     if (abs(test5e) > 4) {
       print("Elec")
       print(as.Date(bldg.Usage.Elec[j,"MeterID"]))
